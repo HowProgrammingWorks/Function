6bbe5ec5cf54ebf55afc679aab26077165b7a578
tshemsedinov <timur.shemsedinov@gmail.com>
2017-02-24 23:54:35 +0200

    JavaScript function default values examples

--- JavaScript/5-default.js
+++ JavaScript/5-default.js
@@ -1,9 +1,36 @@
 'use strict';
 
-function fn(a, b = 'Hello', c = 5) {
+// ES6 style for default parameter values
+//
+function fnNew(a, b = 'Hello', c = 5) {
   console.dir({ a, b, c });
 }
 
-fn(1, 2, 3);
-fn(1, 2);
-fn(1);
+fnNew(1, 2, 3);
+fnNew(1, 2);
+fnNew(1);
+
+// Old style for default parameter values
+//
+function fnOld(a , b, c) {
+  b = b || 'Hello';
+  c = c || 5;
+  console.dir({ a, b, c });
+}
+
+fnOld(1, 2, 3);
+fnOld(1, 2);
+fnOld(1);
+
+// Hash style for default parameter values
+//
+function fnHash(args) {
+  args.a = args.a || [7, 25, 10];
+  args.b = args.b || 'Hello';
+  args.c = args.c || 100;
+  console.dir(args);
+}
+
+fnHash({ a: [1, 2, 3], b: 'Hi', c : 3 });
+fnHash({ b: 'World' });
+fnHash({ c: 7 });
