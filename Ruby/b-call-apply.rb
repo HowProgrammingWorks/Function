d1a4f7fd1e079b8ea6221b266bb895d100413253
VolodymyrBaranivskyi <yamahafz6n@bigmir.net>
2017-05-18 09:23:49 +0300

    Create b-call-apply.rb

---
+++ Ruby/b-call-apply.rb
@@ -0,0 +1,28 @@
+
+class Square
+  def area
+    @side * @side
+  end
+  def initialize(side)
+    @side = side
+  end
+end
+
+area_un = Square.instance_method(:area)
+
+s = Square.new(12)
+area = area_un.bind(s)
+puts area.call   #=> 144
+
+
+
+
+def f1(a,b)
+    puts "f1(#{a}, #{b})"
+end
+
+arr = [1,2]
+
+f1(1,2)
+
+f1(*arr)
