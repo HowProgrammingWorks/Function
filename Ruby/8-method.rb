c2be583b4c00ad2671c0754077cf2ec2da33d24e
VolodymyrBaranivskyi <yamahafz6n@bigmir.net>
2017-05-18 10:07:15 +0300

    Update 8-method.rb

--- Ruby/8-method.rb
+++ Ruby/8-method.rb
@@ -1,1 +1,17 @@
 
+
+word = 'boo'
+word.define_singleton_method(:inc) do |a|
+  return a = a + 1
+end
+puts word.inc(2) 
+#word.singleton_methods(2) 
+word.define_singleton_method(:sum) do |a, b|
+  return a + b
+end
+puts word.sum(2, 1) 
+
+word.define_singleton_method(:max) do |a, b| m =-> (a, b) { a > b ? a : b }
+return m
+end
+puts word.max(2, 1) 
