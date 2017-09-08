495010a3a80d6524702a5637c56ccdaee47d294f
tshemsedinov <timur.shemsedinov@gmail.com>
2017-09-08 11:19:52 +0300

    Change examples

--- JavaScript/9-this.js
+++ JavaScript/9-this.js
@@ -8,8 +8,12 @@ const city = {
   },
   f2: () => {
     return this.name;
+  },
+  f3() {
+    return this.name;
   }
 };
 
 console.log('city.f1() = ' + city.f1());
 console.log('city.f2() = ' + city.f2());
+console.log('city.f3() = ' + city.f3());
