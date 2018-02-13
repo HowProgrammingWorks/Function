cbd98447ac1fc274772dc96076151750efda7ef8
tshemsedinov <timur.shemsedinov@gmail.com>
2018-02-13 17:39:23 +0200

    Add lambda function example

--- JavaScript/1-simple.js
+++ JavaScript/1-simple.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function inc(a) {
-  return ++a;
+  return a + 1;
 }
 
 const sum = function(a, b) {
@@ -10,6 +10,12 @@ const sum = function(a, b) {
 
 const max = (a, b) => (a > b ? a : b);
 
+const avg = (a, b) => {
+  const s = sum(a, b);
+  return s / 2;
+};
+
 console.log('inc(5) = ' + inc(5));
 console.log('sum(1, 3) = ' + sum(1, 3));
 console.log('max(8, 6) = ' + max(8, 6));
+console.log('avg(8, 6) = ' + avg(8, 6));
