d1332ac1f05b4efc726a616edf2fec299a65947d
ser <ser@localhost.localdomain>
2019-10-18 21:49:31 +0300

    update 3, done 4

--- Exercises/3-ip.js
+++ Exercises/3-ip.js
@@ -6,12 +6,12 @@ const ipToInt = (ip = '127.0.0.1') => {
     .map(x => Number.parseInt(x))
     .reduce((acc, val) => {
       for (let j = counter; j > 0; j--) {
-        val = val << 8;
+        val <<= 8;
       }
       counter -= 1;
       return acc + val;
     }, 0);
   return res;
-}
+};
 
 module.exports = { ipToInt };
