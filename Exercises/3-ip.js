699f04845e120141a11b7a48e0e1a9656e8e9bce
ser <ser@localhost.localdomain>
2019-10-19 20:18:15 +0300

    optimized ex#3 with reduce

--- Exercises/3-ip.js
+++ Exercises/3-ip.js
@@ -1,13 +1,10 @@
 'use strict';
 
 const ipToInt = (ip = '127.0.0.1') => {
-  let number;
   const res = ip.split('.')
-    .map(s => Number.parseInt(s));
-  for (let i = 0; i < res.length; i++) {
-    number = (number << 8) + res[i];
-  }
-  return number;
+    .map(s => Number.parseInt(s))
+    .reduce((acc, val) => (acc << 8) + val, 0);
+  return res;
 };
 
 module.exports = { ipToInt };
