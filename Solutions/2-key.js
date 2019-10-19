d2b1f8e2d9a260b336599656416ad06e80412f10
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2019-10-19 21:33:15 +0300

    Remove debug output

--- Solutions/2-key.js
+++ Solutions/2-key.js
@@ -10,8 +10,4 @@ const generateKey = (length, possible) => {
   return key;
 };
 
-const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-const key = generateKey(16, characters);
-console.log(key);
-
 module.exports = { generateKey };
