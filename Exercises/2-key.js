5283aca7440862ac9cb117fa09047f9521129fcf
Станіслав Кулічковський <staskulch@localhost.localdomain>
2019-10-19 15:15:42 +0300

    Add Homework of Function

--- Exercises/2-key.js
+++ Exercises/2-key.js
@@ -1,9 +1,16 @@
 'use strict';
+// Generate string of random characters
+// Use Math.random() and Math.floor()
+// See documentation at MDN
 
 const generateKey = (length, possible) => {
-  // Generate string of random characters
-  // Use Math.random() and Math.floor()
-  // See documentation at MDN
+  let res = '';
+  for (let i = 0; i < length; i++) {
+    const ran = Math.floor(Math.random() * possible.length);
+    const ch = possible[ran];
+    res += ch;
+  }
+  return res;
 };
 
 module.exports = { generateKey };
