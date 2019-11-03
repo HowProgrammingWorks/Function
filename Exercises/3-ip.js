289be5417b4ccae0931013115d80d7452ec39f35
Ivan Opara <oiv84@yandex.ru>
2019-11-03 17:27:12 +0300

    all done

--- Exercises/3-ip.js
+++ Exercises/3-ip.js
@@ -1,11 +1,7 @@
 'use strict';
 
-const ipToInt = (ip = '127.0.0.1') => {
-  // Parse ip address as string, for example '10.0.0.1'
-  // to ['10', '0', '0', '1'] to [10, 0, 0, 1]
-  // and convert to Number value 167772161 with sitwise shift
-  // (10 << 8 << 8 << 8) + (0 << 8 << 8) + (0 << 8) + 1 === 167772161
-  // Use Array.prototype.reduce of for loop
-};
+const ipToInt = (ip = '127.0.0.1') => ip.split('.').reduce(
+  (sum, elem, i, arr) => sum + (Number(elem) << 8 * (arr.length - 1 - i))
+  , 0);
 
 module.exports = { ipToInt };
