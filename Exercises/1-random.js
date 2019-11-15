dead9bfd38e32b0bcf520db979a18b460e685ac0
Oleksandr <sashavytvyckyj@gmail.com>
2019-11-15 17:50:56 +0200

    test

--- Exercises/1-random.js
+++ Exercises/1-random.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const random = (min, max) => {
-  //min = Math.floor(min);
-  //max = Math.floor(max);
+  /// min = Math.floor(min);
+  /// max = Math.floor(max);
   const res = Math.floor(Math.random() * (max - min) + min);
   return res;
 };
