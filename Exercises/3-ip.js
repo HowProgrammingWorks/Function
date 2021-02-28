185a791bdfece88c0ae0976c9077bbc526b936c3
beliy-a <b.a.v.beliy@gmail.com>
2021-02-28 15:53:39 +0300

    add solving for all tasks

--- Exercises/3-ip.js
+++ Exercises/3-ip.js
@@ -1,11 +1,7 @@
 'use strict';
 
-const ipToInt = (ip = '127.0.0.1') => {
-  // Parse ip address as string, for example '10.0.0.1'
-  // to ['10', '0', '0', '1'] to [10, 0, 0, 1]
-  // and convert to Number value 167772161 with bitwise shift
-  // (10 << 8 << 8 << 8) + (0 << 8 << 8) + (0 << 8) + 1 === 167772161
-  // Use Array.prototype.reduce of for loop
-};
+const ipToInt = (ip = '127.0.0.1') => (ip
+  .split('.')
+  .reduce((num, i) => (num << 8) + parseInt(i), 0));
 
 module.exports = { ipToInt };
