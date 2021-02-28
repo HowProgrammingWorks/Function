8ee2b34da2007b81f11c9dee66bfd8a2a3a19d63
beliy-a <b.a.v.beliy@gmail.com>
2021-02-28 16:40:16 +0300

    add solving for all tasks

--- Exercises/1-random.js
+++ Exercises/1-random.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const random = (min, max) => {
-  // Generate random Number between from min to max
-  // Use Math.random() and Math.floor()
-  // See documentation at MDN
+  if (max === undefined) {
+    max = min;
+    min = 0;
+  }
+
+  return Math.floor(Math.random() * (max - min) + min);
 };
 
 module.exports = { random };
--- Exercises/3-ip.js
+++ Exercises/3-ip.js
@@ -1,11 +1,8 @@
 'use strict';
 
 const ipToInt = (ip = '127.0.0.1') => {
-  // Parse ip address as string, for example '10.0.0.1'
-  // to ['10', '0', '0', '1'] to [10, 0, 0, 1]
-  // and convert to Number value 167772161 with bitwise shift
-  // (10 << 8 << 8 << 8) + (0 << 8 << 8) + (0 << 8) + 1 === 167772161
-  // Use Array.prototype.reduce of for loop
+  const ipSplit = ip.split('.');
+  return ipSplit.reduce((acc, val) => (acc << 8) + parseInt(val), 0);
 };
 
 module.exports = { ipToInt };
