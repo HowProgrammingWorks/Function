4966c14daf7ae9655297fd23499c553bbba5ed52
domior <andrew.snisar@gmail.com>
2021-03-02 20:43:15 +0200

    Tasks 1-4 done

--- Exercises/2-key.js
+++ Exercises/2-key.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const generateKey = (length, possible) => {
-  // Generate string of random characters
-  // Use Math.random() and Math.floor()
-  // See documentation at MDN
+  let res = '';
+  const amount = possible.length;
+  for (let i = 0; i < length; i++) {
+    const rand = Math.floor(Math.random() * amount);
+    res += possible[rand];
+  }
+  return res;
 };
 
 module.exports = { generateKey };
--- Exercises/4-methods.js
+++ Exercises/4-methods.js
@@ -1,21 +1,14 @@
 'use strict';
 
 const methods = iface => {
-  // Introspect all properties of iface object and
-  // extract function names and number of arguments
-  // For example: {
-  //   m1: x => [x],
-  //   m2: function (x, y) {
-  //     return [x, y];
-  //   },
-  //   m3(x, y, z) {
-  //     return [x, y, z];
-  //   }
-  // will return: [
-  //   ['m1', 1],
-  //   ['m2', 2],
-  //   ['m3', 3]
-  // ]
+  const arr = [];
+  for (const value in iface) {
+    const func = iface[value];
+    if (typeof func === 'function') {
+      arr.push([value, func.length]);
+    }
+  }
+  return arr;
 };
 
 module.exports = { methods };
