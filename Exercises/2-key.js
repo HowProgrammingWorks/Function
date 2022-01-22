f0ef8f0dca63f7169470e123c0be95dc5f5dd37e
Mitya <partyzan327.gmail.com>
2022-01-22 18:24:08 +0300

    Add Exercises 1-2-3-4

--- Exercises/2-key.js
+++ Exercises/2-key.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const generateKey = (length, possible) => {
-  // Generate string of random characters
-  // Use Math.random() and Math.floor()
-  // See documentation at MDN
+  const max = possible.length;
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += possible[Math.floor(Math.random() * (max - Math.random()))];
+  }
+  return result;
 };
 
 module.exports = { generateKey };
