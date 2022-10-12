299fa585e5f0d803a3884b159667433b7e6ee136
mibal_ua <9mohapx9@gmail.com>
2022-10-12 10:31:50 +0200

    Implement second optimize version
    Optimized by reduce method

--- Exercises/3-ip.js
+++ Exercises/3-ip.js
@@ -1,13 +1,8 @@
 'use strict';
 
 const ipToInt = (ip = '127.0.0.1') => {
-  ip = ip.split('.').map((str) => parseInt(str));
-  let sum = ip[0];
-  for (let i = 1; i < ip.length; i++) {
-    sum <<= 8;
-    sum += ip[i];
-  }
-  return sum;
+  const fn = (previous, current) => (previous << 8) + parseInt(current);
+  return ip.split('.').reduce(fn, 0);
 };
 
 module.exports = { ipToInt };
