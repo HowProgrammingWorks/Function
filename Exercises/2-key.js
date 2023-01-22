33c1aa89f6b25763264534f849dc47c33432b399
Tima Gixe <timagixe@protonmail.com>
2023-01-22 19:17:06 +0200

    Solves 2-key

--- Exercises/2-key.js
+++ Exercises/2-key.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const generateKey = (length, possible) => {
-  // Generate string of random characters
-  // Use Math.random() and Math.floor()
-  // See documentation at MDN
+  const max = possible.length;
+  let key = '';
+  for (let i = 0; i < length; i++) {
+    const index = Math.floor(Math.random() * max);
+    key += possible[index];
+  }
+  return key;
 };
 
 module.exports = { generateKey };
