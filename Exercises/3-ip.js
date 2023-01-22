f11fedbfb0288ec3efd29d2f29e637bdfd653717
Tima Gixe <timagixe@protonmail.com>
2023-01-22 19:17:10 +0200

    Solves 3-ip

--- Exercises/3-ip.js
+++ Exercises/3-ip.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const ipToInt = (ip = '127.0.0.1') => {
-  // Parse ip address as string, for example '10.0.0.1'
-  // to ['10', '0', '0', '1'] to [10, 0, 0, 1]
-  // and convert to Number value 167772161 with bitwise shift
-  // (10 << 8 << 8 << 8) + (0 << 8 << 8) + (0 << 8) + 1 === 167772161
-  // Use Array.prototype.reduce of for loop
-};
+const DOT = '.';
+const BITS = 8;
+const bitwiseSum = (a, b) => (a << BITS) + Number(b);
+
+const ipToInt = (ip = '127.0.0.1') => ip
+  .split(DOT)
+  .reduce(bitwiseSum);
 
 module.exports = { ipToInt };
